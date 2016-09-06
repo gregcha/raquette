8395d3866094758ff97d677c4201378d591a7abc
gregoire charles <gcm.charles@gmail.com>
2016-09-07 00:27:15 +0200

    add fullscreen menu component to booking process

--- app/assets/javascripts/app/navbar.js
+++ app/assets/javascripts/app/navbar.js
@@ -10,56 +10,58 @@ $(document).ready(function(){
     rotateX: '35deg'
   }, 0);
 
-  $('.toggler').click(function()
-                    {
-    if ( ++count % 2 === 1 )
-    {
-      // Show
-      $overlay
-        .velocity('stop')
-        .velocity('fadeIn', 300);
-
-      $links
-        .velocity('stop')
-        .velocity({
-          opacity: 1,
-          translateY: '-50%',
-          translateX: '-50%',
-          rotateX: '0deg'
-        }, 500);
-    }
-    else
-    {
-      // Hide
-      $links
-        .velocity('stop')
-        .velocity({
-          opacity: 0.4,
-          translateY: '-25%',
-          rotateX: '-35deg'
-        },
-        {
-          duration: 250,
-          complete: function(elements)
-          {
-            $links.velocity({
-              translateY: '-75%',
-              rotateX: '35deg'
-            }, 0);
-          }
-        });
-
-      $overlay
-        .velocity('stop')
-        .velocity('fadeOut', {
-          duration: 200
-        });
-    }
-
-    return false;
+  $('.open-toggler').click(function(){
+    var $color = $(this).attr('class').split(' ')[0];
+    $overlay
+      .addClass($color)
+      .velocity('stop')
+      .velocity('fadeIn', 300);
+    $links
+      .velocity('stop')
+      .velocity({
+        opacity: 1,
+        translateY: '-50%',
+        translateX: '-50%',
+        rotateX: '0deg'
+      }, 500);
   });
 
-  setTimeout(function(){
-    $('.toggler').trigger('click');
-  }, 500);
+  $('.close-toggler').click(function(){
+    $links
+      .velocity('stop')
+      .velocity({
+        opacity: 0.4,
+        translateY: '-25%',
+        rotateX: '-35deg'
+      },
+      {
+        duration: 250,
+        complete: function(elements)
+        {
+          $links.velocity({
+            translateY: '-75%',
+            rotateX: '35deg'
+          }, 0);
+        }
+      });
+
+    var $color = $overlay.attr('class').split(' ')[1];
+    $overlay
+      .velocity('stop')
+      .velocity('fadeOut', {
+        duration: 200
+      })
+      .removeClass($color);
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
